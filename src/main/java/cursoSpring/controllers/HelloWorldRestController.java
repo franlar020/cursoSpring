f7d4412b0557b2345fd012199c63f0e560655ffe
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-04-12 20:31:37 -0300

    crecion del proyecto

---
+++ src/main/java/cursoSpring/controllers/HelloWorldRestController.java
@@ -0,0 +1,15 @@
+package cursoSpring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping({"/hello", "/hw", "/hola", "/fr"})
+
+    public String helloWorld (){
+        System.out.println("Solicitud ejecutada!");
+        return "Hello, World, Aloha";
+    }
+}
